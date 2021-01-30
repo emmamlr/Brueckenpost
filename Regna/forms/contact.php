a26902a6bfe8244d7a5ff4cdeb533d45b3cc54f8
emmamlr <77997527+emmamlr@users.noreply.github.com>
2021-01-30 13:00:27 +0100

    Update contact.php

--- Regna/forms/contact.php
+++ Regna/forms/contact.php
@@ -7,11 +7,10 @@
     
     $message = $_POST['message'];
     $mailto = "emmamarlen03@outlook.de";
-    $headers = "Gesendet von: ".$name;
-    $text = "Sie haben eine E-Mail von ".$mailfrom." erhalten!\n\n".$message;
+    $headers = 'Gesendet von: '.$name;
+    $text = 'Sie haben eine E-Mail von '.$mailfrom.' erhalten!\n\n'.$message;
 
     mail($mailto, $subject, $text, $headers);
-    header("Location: index.php?mailsend");
   }
 
 ?>
